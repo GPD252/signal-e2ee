9d6ebec780d1d6b1faec4bb82b59806069843d2c
GPD252 <gitprojectdeveloper@outlook.com>
2026-02-01 18:42:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,41 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
 
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const preKeys = {}; // public prekeys only
+
+wss.on("connection", ws => {
+  ws.on("message", msg => {
+    const data = JSON.parse(msg);
+
+    // Store public prekeys
+    if (data.type === "prekey") {
+      preKeys[data.user] = data.prekey;
+      return;
+    }
+
+    // Fetch recipient prekey
+    if (data.type === "getPrekey") {
+      ws.send(JSON.stringify({
+        type: "prekey",
+        prekey: preKeys[data.to]
+      }));
+      return;
+    }
+
+    // Relay encrypted messages (server cannot read)
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === 1) {
+        client.send(msg.toString());
+      }
+    });
+  });
+});
+
+server.listen(3000, () =>
+  console.log("Signal-style server running on http://localhost:3000")
+);
