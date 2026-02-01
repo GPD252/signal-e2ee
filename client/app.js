1f9725d4df7a3c0005ce4693c9e96554c80d3e9c
GPD252 <gitprojectdeveloper@outlook.com>
2026-02-01 18:55:02 +0530

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,71 +1,12 @@
-import {
-  createIdentity,
-  deriveSecret,
-  ratchet,
-  encrypt,
-  decrypt
-} from "./crypto.js";
+import { startCall, handleSignal } from "./webrtc.js";
 
 const ws = new WebSocket("ws://localhost:3000");
-let identity, chainKey, username;
 
 ws.onmessage = async e => {
   const data = JSON.parse(e.data);
-  if (data.payload && chainKey) {
-    const r = await ratchet(chainKey);
-    chainKey = r.nextChainKey;
-    const text = await decrypt(r.msgKey, data.payload);
-    log(text);
-  }
+  await handleSignal(data, ws);
 };
 
-window.init = async () => {
-  username = document.getElementById("user").value;
-  identity = await createIdentity();
-
-  const pub = await crypto.subtle.exportKey(
-    "raw",
-    identity.publicKey
-  );
-
-  ws.send(JSON.stringify({
-    type: "prekey",
-    user: username,
-    prekey: [...new Uint8Array(pub)]
-  }));
-
-  log("Identity created");
+window.call = async () => {
+  await startCall(ws);
 };
-
-window.send = async () => {
-  const msg = document.getElementById("msg").value;
-
-  ws.send(JSON.stringify({
-    type: "getPrekey",
-    to: username
-  }));
-
-  ws.onmessage = async e => {
-    const data = JSON.parse(e.data);
-    if (data.prekey) {
-      const pubKey = await crypto.subtle.importKey(
-        "raw",
-        new Uint8Array(data.prekey),
-        { name: "ECDH", namedCurve: "X25519" },
-        false,
-        []
-      );
-
-      chainKey = await deriveSecret(identity.privateKey, pubKey);
-      const r = await ratchet(chainKey);
-      chainKey = r.nextChainKey;
-
-      const payload = await encrypt(r.msgKey, username + ": " + msg);
-      ws.send(JSON.stringify({ payload }));
-    }
-  };
-};
-
-function log(t) {
-  document.getElementById("log").textContent += t + "\n";
-}
